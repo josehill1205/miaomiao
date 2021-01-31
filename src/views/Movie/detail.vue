5f700c4a5529be438ca1e851f86f9ae21d54f683
Json <josehill1205@hotmail.com>
2021-01-31 16:59:37 +0800

    bug clear

--- src/views/Movie/detail.vue
+++ src/views/Movie/detail.vue
@@ -40,7 +40,7 @@
 
         <div v-else id="content" class="contentDetail">
             <div class="detail_list">
-                <div class="detail_list_bg"></div>
+                <div class="detail_list_bg" :style="{ 'background-image':'url('+ detailMovie.img.replace(/w\.h/,'148.208') +')'"}"></div>
                 <div class="detail_list_filter"></div>
                 <div class="detail_list_content">
                     <div class="detail_list_img">
@@ -125,7 +125,7 @@ export default {
 }
 #content.contentDetail{ display:block; margin-bottom:0;}
 #content .detail_list{ height:200px; width:100%; position: relative; overflow:hidden;}
-.detail_list .detail_list_bg{width:100%; height:100%; background:url(/images/龙猫1.jpg) 0 40%;filter:blur(200px);}
+.detail_list .detail_list_bg{width:100%; height:100%; background: 0 40%;filter:blur(20px);background-size:cover; position:absolute; left:0; top:0;}
 .detail_list .detail_list_filter{ width:100%; height:100%;position:absolute; background-color:#40454d;opacity:0.5;}
 .detail_list .detail_list_content{display:flex; width:100%; height:100%; position:absolute; left:0; top:0; z-index:50;}
 .detail_list .detail_list_img{width:108px; height:150px; border:1px #f0f2f3 solid; margin:20px;}
